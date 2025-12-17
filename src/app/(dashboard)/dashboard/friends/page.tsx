da803336198dcf66dae2df17a23a8009a3b37643
nnyPRO <nanmesanee.17@gmail.com>
2025-12-17 23:45:00 +0700

    add friends ui

--- src/app/(dashboard)/dashboard/friends/page.tsx
+++ src/app/(dashboard)/dashboard/friends/page.tsx
@@ -1,3 +1,21 @@
+import AddFriendForm from "@/components/AddFriendForm";
+
 export default function FriendsPage() {
-    return <div>friends Page</div>
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const formData = new FormData(event.currentTarget);
+        const email = formData.get('email'); // Retrieving values ​from a TextField by name.
+        console.log("Adding friend with email:", email);
+
+        // await fetch('/api/friends', { method: 'POST', body: JSON.stringify({ email }) });
+    };
+
+    return <>
+        <h1>Add friends</h1>
+        <p>Add friends by email</p>
+
+        <AddFriendForm />
+    </>
 }
